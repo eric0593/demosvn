2451679d1e9f14f0563117c7f37c4a240dcf026e
wulongcd <2953453733@qq.com>
2018-07-10 18:20:59 +0800

    代码微调，功能无关

--- source/app/src/main/java/com/idea/jgw/ui/mining/IdentityAuthentication2Activity.java
+++ source/app/src/main/java/com/idea/jgw/ui/mining/IdentityAuthentication2Activity.java
@@ -42,13 +42,12 @@ import java.io.File;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-import static com.idea.jgw.RouterPath.IDENTITY_AUTHENTICATION_ACTIVITY2;
 import static com.idea.jgw.api.OkhttpApi.BASE_HOST;
 
 /**
  * 实名认证，第二步，上传照片
  */
-@Route(path = IDENTITY_AUTHENTICATION_ACTIVITY2)
+@Route(path = RouterPath.IDENTITY_AUTHENTICATION_ACTIVITY2)
 public class IdentityAuthentication2Activity extends BaseActivity {
     //调用系统相机请求码，正面
     public static final int DO_CAMERA_REQUEST = 100;
